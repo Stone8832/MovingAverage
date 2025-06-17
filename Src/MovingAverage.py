eee455d6e383dae135b54eb3baf13490b4678263
Stone8832 <willrule88@gmail.com>
2025-06-17 00:13:17 -0700

    5/16 12:13

--- Src/MovingAverage.py
+++ Src/MovingAverage.py
@@ -31,11 +31,74 @@ spy['Strategy EQ Return'] = (1 + spy['Daily Strategy Returns Pct']).cumprod()
 #buy and hold returns
 spy['Hold EQ Return'] = (1 + spy['Spy Daily Pct']).cumprod()
 
-#Plot
+#Plot SPY and moving averages
 spy[['Close', 'ma_short', 'ma_long']].plot(title = 'SPY Price Chart and Moving Averages')
-spy[['Hold EQ Return', 'Strategy EQ Return']].plot(title = 'SPY Trading Strategy Equity Comparison');plt.show()
+plt.xlabel('Date')
+plt.ylabel('Price')
+
+#Plot EQ Returns
+spy[['Hold EQ Return', 'Strategy EQ Return']].plot(title = 'SPY Trading Strategy Equity Comparison')
+plt.xlabel('Date')
+plt.ylabel('Equity')
+
+#plt.show() uncomment this to bring graphs back
+
 
 #Notes: Need to make graphs prettier and define financial metrics and possibly test multiple rolling windows
+
+#Calculate CAGR: Compound annual growth rate, provides smooth year-over-year growth return
+def calc_cagr(equity):
+    ending_value = equity.iloc[-1]
+    days = len(equity)
+    annual_factor = 252 / days
+    return (ending_value ** annual_factor) - 1
+
+#Calculate Annualized volatility: Measure of how volatile an investment is over a year
+def calc_av(returns):
+    daily_variance = returns.std()
+    return daily_variance * np.sqrt(252)
+
+#Calculate Sharpe Ratio: Return per unit of risk "How much extra return did I get for each percent of volatility I endured". Note, we assume zero risk free rate
+def calc_sharpe(cagr, vol):
+    return cagr / vol if vol != 0 else np.nan
+
+#Calculate Maximum drawdown: The worst peak-to- trough decline our equity curve experienced
+def calc_drawdown(equity):
+    peak = equity.cummax() #returns a series with the max up until that point
+    drawdown = (equity - peak) / peak
+    return drawdown.min()
+
+
+
+#Compute metrics for buy and hold
+hold_cagr = calc_cagr(spy['Hold EQ Return'])
+hold_av = calc_av(spy['Spy Daily Pct'])
+hold_sharpe = calc_sharpe(hold_cagr, hold_av)
+hold_drawdown = calc_drawdown(spy['Hold EQ Return'])
+
+#Compute metrics for trading strategy
+str_cagr = calc_cagr(spy['Strategy EQ Return'])
+str_av = calc_av(spy['Daily Strategy Returns Pct'])
+str_sharpe = calc_sharpe(str_cagr, str_av)
+str_drawdown = calc_drawdown(spy['Strategy EQ Return'])
+
+#Print and compare the financial metrics
+print("Metric                 |     Buy-and-hold       |     Trading Strategy ")
+print("-----------------------|------------------------|----------------------")
+print(f"CAGR                  |    {hold_cagr: .2%}     |   {str_cagr: .2%}     ")
+print(f"Annualized Volatility |    {hold_av: .2%}       |   {str_av: .2%}       ")
+print(f"Sharpe Ratio          |    {hold_sharpe: .2f}   |   {str_sharpe: .2f}   ")
+print(f"Maximum Drawdown      |    {hold_drawdown: .2%} |   {str_drawdown: .2%} ")
+
+#Comparision
+if hold_sharpe > str_sharpe:
+    print("\nBuy-and-hold edges out the trading strategy on risk-adjusted returns based on the Sharpe ratio.")
+else:
+    print("\nThe trading strategy out performs the simply buy-and-hold strategy for the S&P500 based on the Sharpe ratio.")
+
+
+
+
 
 
 
